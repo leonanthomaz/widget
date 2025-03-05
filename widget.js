415b527857ab0847640ad1d7252bfa63be8ccc87
Leonan <79343444+leonanthomaz@users.noreply.github.com>
2025-03-05 00:17:20 -0300

    upgrade endpoint

--- widget.js
+++ widget.js
@@ -224,7 +224,7 @@
 
     showTypingIndicator(true);
 
-    const response = await fetch("http://localhost:8000/chat", {
+    const response = await fetch("https://genius-43l4.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message })
